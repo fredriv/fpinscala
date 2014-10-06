729563512d571e9b4314e95e2db8dac01b156120
Fredrik Vraalsen <fredrik@vraalsen.no>
2014-10-06 18:51:00 +0200

    Exercise 2.2

---
+++ src/main/scala/Exercise-2-2.scala
@@ -0,0 +1,10 @@
+object Exercise_2_2 {
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean =
+      if (n >= as.size) true
+      else ordered(as(n - 1), as(n)) && loop(n + 1)
+
+    loop(1)
+  }
+}
