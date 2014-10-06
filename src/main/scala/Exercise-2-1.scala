95303adf767574a460f388caa4a3c3a791ae8967
Fredrik Vraalsen <fredrik@vraalsen.no>
2014-10-06 18:23:49 +0200

    Exercise 2.1

---
+++ src/main/scala/Exercise-2-1.scala
@@ -0,0 +1,23 @@
+object MyModule {
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else loop(n - 1, n * acc)
+
+    loop(n, 1)
+  }
+
+  def fib(n: Int): BigInt = {
+    @annotation.tailrec
+    def loop(n: Int, a: BigInt, b: BigInt): BigInt =
+      if (n <= 1) a
+      else loop(n - 1, b, a + b)
+
+    loop(n, 0, 1)
+  }
+
+  def main(args: Array[String]): Unit =
+    println(factorial(5))
+}
